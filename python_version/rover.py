4f4b72579c9258e0b307b741b277a89b45bb3926
Kenny William Nyallau <k3nnywilliam@gmail.com>
2020-02-21 18:28:09 +0100

    Clean refactor

--- python_version/rover.py
+++ python_version/rover.py
@@ -5,24 +5,52 @@ This is a python implementation of Rover challenge
 import re
 
 class Rover:
-
-    def __init__(self, instruction, orientation, x, y, px, py):
-        self.instruction = instruction.upper()
+    def __init__(self):
+        self.instruction = ""
         self.__arr_instruction = []
-        self.orientation = orientation.upper()
-        self.x = int(x)
-        self.y = int(y)
-        self.__save_x = x
-        self.__save_y = y
-        self.px = int(px)
-        self.py = int(py)
+        self.orientation = ""
+        self.x = 0
+        self.y = 0
+        self.__save_x = 0
+        self.__save_y = 0
+        self.px = 0
+        self.py = 0
+        self.name =""
+
+    def setRoverName(self):
+        name = input("Please name your Rover:")
+        self.name = name
 
     
-    def checkValidInstruction(self):
+    def setCoordinatesAndOrientation(self):
+        
+        while(True):
+            
+            x, y, orientation = input(f"Please enter {self.name}'s x,y, orientation:").split()
+
+            self.x = int(x)
+            self.y = int(y)
+            self.orientation = orientation.upper()
+
+            valid_orientation = "NSWEnswe"
+            ori = re.findall(orientation, valid_orientation)
+            if(int(x) < 0 or int(y) < 0):
+                print("Invalid coordinates!")
+            elif(int(x) >=0 and ori or int(y) >=0 and ori):
+                break
+            else:
+                print("Invalid coordinates and/or orientation!")
+
+    
+    def setInstruction(self):
         
         cond = True
         
         while(cond):
+
+            instruction = input(f"Please enter {self.name}'s' instruction:")
+
+            self.instruction = instruction.upper()
             valid_instruction = "LRMlrm"
 
             self.__arr_instruction = [self.__arr_instruction 
@@ -38,6 +66,9 @@ class Rover:
                     instruction = input("Please enter your instruction:")
                     self.instruction = instruction
 
+    def setPlateauCoordinates(self, px, py):
+        self.px = px
+        self.py = py
 
     def __parseInstruction(self):
         self.__arr_instruction = [self.__arr_instruction 
